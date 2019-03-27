5f50a1974ce240f756df1d6040f495686443b485
JenyaKapul <37771786+JenyaKapul@users.noreply.github.com>
2019-03-27 14:53:42 +0200

    database clases

---
+++ CODE/app/src/main/java/com/example/user/skillbarter/UserSkills.java
@@ -0,0 +1,61 @@
+package com.example.user.skillbarter;
+
+import com.google.common.primitives.UnsignedInteger;
+
+public class UserSkills {
+
+    private String  userID;
+    private String skillId;
+    private int level;
+    private double ranking;
+    private int pointsValue;
+    private String address;
+
+    public UserSkills(String userID, String skillId, int level, int pointsValue, String address) {
+        this.userID = userID;
+        this.skillId = skillId;
+        this.level = level;
+        this.pointsValue = pointsValue;
+        this.address = address;
+    }
+
+    public UserSkills(String userID, String skillId, int level, int pointsValue) {
+        this.userID = userID;
+        this.skillId = skillId;
+        this.level = level;
+        this.pointsValue = pointsValue;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(double ranking) {
+        this.ranking = ranking;
+    }
+
+    public int getPointsValue() {
+        return pointsValue;
+    }
+
+    public void setPointsValue(int pointsValue) {
+        this.pointsValue = pointsValue;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
