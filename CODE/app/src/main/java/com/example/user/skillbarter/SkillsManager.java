00100a252adca52121fbca0767a5e07840c5b275
Noa <noabinen@gmail.com>
2019-04-01 19:20:10 +0300

    added functionality for spinners in SkillsManager intent

--- CODE/app/src/main/java/com/example/user/skillbarter/SkillsManager.java
+++ CODE/app/src/main/java/com/example/user/skillbarter/SkillsManager.java
@@ -1,17 +1,14 @@
 package com.example.user.skillbarter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class SkillsManager extends ActionBarMenuActivity
-        implements AdapterView.OnItemSelectedListener {
+public class SkillsManager extends ActionBarMenuActivity {
 
     private static final String TAG = "SkillsManager";
 
@@ -22,36 +19,47 @@ public class SkillsManager extends ActionBarMenuActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skills_manager);
 
-        mMainSpinner = findViewById(R.id.category_spinner);
-        mSecondarySpinner = findViewById(R.id.skills_spinner);
-
-        // add listener to the first spinner in order to load the correct data
-        // for secondary spinner.
-        mMainSpinner.setOnItemSelectedListener(this);
-
-        mSecondarySpinner.setOnItemSelectedListener(this);
-
         List<CharSequence> categories = Arrays.asList(this.getResources().getTextArray(R.array.skills_categories));
 
         HintAdapter adapter = new HintAdapter(this, categories,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        mMainSpinner = findViewById(R.id.category_spinner);
         mMainSpinner.setAdapter(adapter);
 
         // show hint
         mMainSpinner.setSelection(adapter.getCount());
+
+        // add listener to the first spinner in order to load the correct data
+        // for secondary spinner.
+        mMainSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                firstSpinnerChooser(parent, view, position, id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        mSecondarySpinner = findViewById(R.id.skills_spinner);
+        mSecondarySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                secondSpinnerChooser(parent, view, position, id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    private void firstSpinnerChooser(AdapterView<?> parent, View view, int position, long id) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
 
         int skillArrayID = R.array.Empty;
         String categoryLabel = parent.getItemAtPosition(position).toString();
-
-        Toast.makeText(this, categoryLabel, Toast.LENGTH_SHORT).show();
 
         switch (categoryLabel) {
             case "Tutoring":
@@ -90,15 +98,18 @@ public class SkillsManager extends ActionBarMenuActivity
         HintAdapter adapter = new HintAdapter(this, skillsList,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        
         mSecondarySpinner.setAdapter(adapter);
 
         // show hint.
         mSecondarySpinner.setSelection(adapter.getCount());
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+    private void secondSpinnerChooser(AdapterView<?> parent, View view, int position, long id) {
+        String skillLabel = parent.getItemAtPosition(position).toString();
+        if (!skillLabel.startsWith("Choose")) {
+            findViewById(R.id.button_add).setEnabled(true);
+            findViewById(R.id.button_add).setBackgroundColor(0xFF039BE5); // primary background color
+        }
     }
 }
