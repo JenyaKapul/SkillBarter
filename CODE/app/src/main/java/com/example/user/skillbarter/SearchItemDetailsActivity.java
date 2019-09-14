b93b519bd856d1e0a99f89548c164480891bc4b8
JenyaKapul <37771786+JenyaKapul@users.noreply.github.com>
2019-09-14 17:34:23 +0300

    SearchItemDetailsActivity back menu support

--- CODE/app/src/main/java/com/example/user/skillbarter/SearchItemDetailsActivity.java
+++ CODE/app/src/main/java/com/example/user/skillbarter/SearchItemDetailsActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -65,6 +68,24 @@ public class SearchItemDetailsActivity extends AppCompatActivity {
         if (extras != null) {
             this.setDataFromSkillData(extras.getString(KEY_SKILL_ID));
 //            this.setDataFromUserData(userID);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.back_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.back_navigation:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
