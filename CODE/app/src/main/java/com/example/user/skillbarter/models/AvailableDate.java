3784904d8ce52ec48b0687226f1320887a15366f
Noa <noabinen@gmail.com>
2019-10-14 17:08:26 +0300

    Removed available field from AvailableDate object

--- CODE/app/src/main/java/com/example/user/skillbarter/models/AvailableDate.java
+++ CODE/app/src/main/java/com/example/user/skillbarter/models/AvailableDate.java
@@ -3,14 +3,12 @@ package com.example.user.skillbarter.models;
 import java.util.Date;
 public class AvailableDate {
     private Date date;
-    private boolean available;
     private boolean booked;
 
     public AvailableDate() {}
 
     public AvailableDate(Date date) {
         this.date = date;
-        this.available = true;
         this.booked = false;
     }
 
@@ -20,14 +18,6 @@ public class AvailableDate {
 
     public void setDate(Date date) {
         this.date = date;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
     }
 
     public boolean isBooked() {
